66608e59a8da249e4f540a039255bd080a03d338
Henrique Silverio <henrique.silverio@db1.com.br>
2018-06-22 18:09:00 -0300

    Funções: busca em largura e inclusão de aresta de retorno

--- main.py
+++ main.py
@@ -8,16 +8,64 @@ fonte = definicoes[0]
 sumidouro = definicoes[1]
 arquivo.close()
 grafo = {}
-grafo[fonte] = {}
-grafo[sumidouro] = {}
+grafo[fonte] = {
+    "nome": fonte,
+    "cor": "branco",
+    "adjacentes": {},
+    "predecessor": {}
+}
+grafo[sumidouro] = {
+    "nome": sumidouro,
+    "cor": "branco",
+    "adjacentes": {},
+    "predecessor": {}
+}
+
+# Construindo o grafo
+
 
 def criarGrafo(linhas):
     for linha in linhas:
         vertices = linha.split(' ')
         if vertices[0] in grafo and type(grafo[vertices[0]]) is dict:
-            grafo[vertices[0]][vertices[1]] = vertices[2]
+            grafo[vertices[0]]["adjacentes"][vertices[1]] = int(vertices[2])
         else:
-            grafo[vertices[0]] = { vertices[1]: vertices[2] }
+            grafo[vertices[0]] = {
+                "nome": vertices[0],
+                "cor": "branco",
+                "adjacentes": {vertices[1]: int(vertices[2])},
+                "predecessor": {}
+            }
+
+# Inclui arestas de avanço
+
+
+def incluiArestaDeAvanco(grafo):
+    for vertice in grafo.keys():
+        for adjacente in grafo[vertice]["adjacentes"].keys():
+            if not vertice in grafo[adjacente]["adjacentes"]:
+                grafo[adjacente]["adjacentes"][vertice] = 0
+
 
 criarGrafo(linhas)
-print(grafo)
+incluiArestaDeAvanco(grafo)
+
+# Busca em largura
+# Calcular a distancia entre dois vertices
+
+
+def WFS(grafo, verticeInicial):
+    fila = []
+    verticeInicial["cor"] = "cinza"
+    fila.append(verticeInicial)
+    while(len(fila) != 0):
+        vertice = fila.pop(0)
+        for key in vertice["adjacentes"].keys():
+            if grafo[key]["cor"] == "branco":
+                grafo[key]["cor"] = "cinza"
+                grafo[key]["predecessor"] = vertice
+                fila.append(grafo[key])
+        vertice["cor"] = "preto"
+
+
+# WFS(grafo, grafo[fonte])
