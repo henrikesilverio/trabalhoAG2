edf7ba3033857c9769bbf03037dfe395be86c043
henrikesilverio <henrikesilverio@users.noreply.github.com>
2018-06-21 22:48:28 -0300

    Inclusão do arquivos de exemplo e função de criação do grafo

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("test")
+arquivo = open("exemplo1.txt")
+
+linhas = arquivo.read().split("\n")
+definicoes = linhas[0].split(' ')
+linhas = linhas[1:]
+
+fonte = definicoes[0]
+sumidouro = definicoes[1]
+arquivo.close()
+grafo = {}
+grafo[fonte] = {}
+grafo[sumidouro] = {}
+
+def criarGrafo(linhas):
+    for linha in linhas:
+        vertices = linha.split(' ')
+        if vertices[0] in grafo and type(grafo[vertices[0]]) is dict:
+            grafo[vertices[0]][vertices[1]] = vertices[2]
+        else:
+            grafo[vertices[0]] = { vertices[1]: vertices[2] }
+
+criarGrafo(linhas)
+print(grafo)
